c4a759e169889198fae17dc0618c42fd30ab4f47
Danielle Jones <daniellejones_97@yahoo.com>
2023-08-29 15:42:36 +0100

    model created for prompt list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_144055) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_144213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_144055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["emoji_id"], name: "index_playlists_on_emoji_id"
+  end
+
+  create_table "prompts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
